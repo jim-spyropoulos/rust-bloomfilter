2ce2cf0e63fddc58018fc2432192481db6a39f31
Dimitris Spyropoulos <dimitrisspyr@gmail.com>
2019-07-03 15:04:40 +0300

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,12 +1,7 @@
 extern crate cc;
 
 fn main() {
-    // cc::Build::new()
-    //     .file("src/double.c")
-    //     .compile("libdouble.a");
     
-    //println!("cargo:rustc-link-search=/Users/jJimo/Downloads/xxHash-dev/");
-
       cc::Build::new()
           .file("src/xxhash.c")
           .compile("libdouble.a");
