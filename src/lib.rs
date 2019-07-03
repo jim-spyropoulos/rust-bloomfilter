87287f9aa43c982de8b7515c7d34137f8038b8ac
Dimitris <dimitrisspyr@gmail.com>
2019-07-03 14:29:36 +0300

    Comments added

--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,14 @@
-//for c functions
 extern crate libc;
-
-
-
-use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
-
-use std::default::Default;
-use std::hash::Hasher;
 use bit_vec::BitVec;
 
 
+
+//Hash functions that were used in the bloom filter:
+//XXH64 taken from https://github.com/Cyan4973/xxHash
+//A simle, yet fast mix-hash function
+
 extern {
-            //fn double_input(input: libc::c_ulonglong) -> libc::c_ulonglong;
             fn XXH64(num: *mut ::libc::c_void, length: libc::size_t, seed: libc::c_ulonglong) -> libc:: c_ulonglong;
-         }
-
-/// An implementation of the Fowler–Noll–Vo hash function.
-
-pub struct FNVHasher{
-    fnv_offset_basis: u64 ,
-    fnv_prime: u64,
-    state: u64
-}
-
-impl Default for FNVHasher {
-    fn default() -> Self{
-        FNVHasher{ //for 64-bit hashes
-            fnv_offset_basis : 0xcbf29ce484222325,
-            fnv_prime : 1099511628211,
-            state : 0xcbf29ce484222325
-        }
-    }
-}
-
-impl FNVHasher {
-    pub fn new() -> Self{
-        FNVHasher{
-            ..FNVHasher::default()
-        }
-    }
-}
-
-impl Hasher  for FNVHasher{
-    fn finish(&self) -> u64 {
-        self.state
-    }
-
-    fn write(&mut self, bytes: &[u8]){
-
-        for element in bytes.iter() {
-            //for every byte
-            self.state = self.state.wrapping_mul(self.fnv_prime);
-            self.state ^= *element as u64; // as &u32;
-        }
-     }
-}
-impl FNVHasher {
-    pub fn reset_hasher(&mut self){
-        self.state = self.fnv_offset_basis;
-    }
 }
 
 //A simple hash mix function for u64
@@ -74,45 +23,51 @@ pub struct MyBloomFilter{
     bit_table : BitVec,
     hash_functions: u32, //number of hash functions
     size : u64 ,  //size of bloom filter 
-    //error_prob : f64  //probability of errors
 }
+ // Make the size of bloom filter equal to the nearest power of 2.
+ // So, the expensive modulo operation during insert, is replaced by a bitwise and with a bitmask(that equals to bloom_size - 1 )
+ 
+ pub fn modify_bloom_size(size_bloom : &f64) -> f64{
 
- pub fn clever_bloom_size(size_bloom : &f64) -> f64{
         let l2 = size_bloom.log2(); //f64
         let d_floor = size_bloom - (2.0 as f64).powf(l2.floor()); //previous lower integer
         let d_ceil =  (2.0 as f64).powf(l2.ceil()) - size_bloom ; //next integer
-       // println!("Log2 of {} is {}.d_floor {} d_ceil {} ",size_bloom,l2,d_floor,d_ceil);
-        let mut ret:f64 = 0.0;
-        if (d_floor < d_ceil){
-            ret = (2.0 as f64).powf(l2.floor())
+        if d_floor < d_ceil{ 
+            (2.0 as f64).powf(l2.floor())
         }
         else{
-            ret = (2.0 as f64).powf(l2.ceil())
+            (2.0 as f64).powf(l2.ceil())
         }
-        println!("Received {} . returning {}",size_bloom, ret );
-        //println!("bitand 13 & 4 = {}",13&4);
-        ret
+        
  }
 
  
 
 impl MyBloomFilter{
     
-   
+   // This function, given the number of elements to index and the desired error probability constructs a bloom filter.
+   // To do so, it uses the theroretical formulas. See: https://en.wikipedia.org/wiki/Bloom_filter
+   // Num_hashes is the number of hash functions to be used.
+
     pub fn create(elements:u64, error_prob: f64) -> Self{
-        let mut size_bloom = - (elements as f64* error_prob.ln()) / (2.0 as f64).ln().powf(2.0 as f64) ;
-        let s2 = clever_bloom_size(&size_bloom); //size of bloom filter as power of 2
-        let num_hashes = ( s2/elements as f64 ) * (2.0 as f64).ln();
+        let size_bloom = - (elements as f64* error_prob.ln()) / (2.0 as f64).ln().powf(2.0 as f64) ;
+        let s2 = modify_bloom_size(&size_bloom); //size of bloom filter as power of 2
+        let num_hashes = ( s2.ceil()/elements as f64 ) * (2.0 as f64).ln();
         println!("Creating a bloom filter with {} bits and {} hash functions",s2.ceil(),num_hashes.ceil());
         MyBloomFilter{
-            bit_table : BitVec::from_elem(s2 as usize, false),
+            bit_table : BitVec::from_elem(s2.ceil() as usize, false),
             hash_functions : num_hashes.ceil() as u32,
-            size : s2 as u64,//size_bloom.ceil() as u64,
+            size : s2.ceil() as u64
         }
     }
 
-   
-    pub fn clever_insert_bloom(&mut self, number : u64){
+   // Function insert_bloom performs the insertions to the bloom filter, by triggering the bit pointed by the respective hash function.
+   // Instead of calling consecutively the hash functions with pipelined input Kirsch-Mitzenmacher-Optimization is used.
+   // See : Kirsch-Mitzenmacher-Optimization https://www.eecs.harvard.edu/~michaelm/postscripts/tr-02-05.pdf
+   // For every element we produce the mix_hash and XXH64 hash keys using the respective functions.
+   // XXH64 is externed from the C binary with zero overhead.
+
+    pub fn insert_bloom(&mut self, number : u64){
         let mixh :u64 = mix_hash(&number);
        
         let mut num = number;
@@ -123,44 +78,20 @@ impl MyBloomFilter{
         let mut h_value:u64 = mixh;
 
         self.bit_table.set((h_value & self.size-1) as usize, true);
-        for elem in 1 .. self.hash_functions {
+        for _elem in 1 .. self.hash_functions {
             h_value = h_value.wrapping_add(xx_hash);
             self.bit_table.set( (h_value & self.size-1) as usize,true);
-            //self.bit_table.set( ( mixh.wrapping_add(xx_hash.wrapping_mul(elem as u64))  & self.size-1 )as usize, true);
         }
     }
-    pub fn insert_bloom(&mut self, number : u64){
-        
-        // let mut h = FNVHasher::new();
-        // h.write(&number.to_be_bytes());
-        // let fnv_hash:u64 = h.finish();
-
-       let mixh :u64 = mix_hash(&number);
-       
-        let mut num = number;
-        let my_ref : *mut libc::c_void = &mut num as *mut _  as *mut libc::c_void;
-        let xx_hash  = unsafe { XXH64(my_ref,8,1994) };
-
-        //to avoid constant multiplications 
-        let mut h_value:u64 = mixh;
-        self.bit_table.set((h_value % self.size) as usize, true);
-        for elem in 1 .. self.hash_functions {
-            h_value = h_value.wrapping_add(xx_hash);
-            
-            self.bit_table.set( ( h_value % self.size )as usize, true);
-        
-        }
-    }
-
-
+   
+    // Check_bloom function checks the posibility of an element's existence in the set.
+    // If at most 1 hash functions points to an unitialized bit check_bloom return false.
+    // Else, the element belongs to the set possibly.
 
     pub fn check_bloom(&self,number : u64) -> bool{
         let mut flag = true;
 
-        // let mut h = FNVHasher::new();
-        // h.write(&number.to_be_bytes());
-        // let fnv_hash:u64 = h.finish();
-
+     
         let mixh :u64 = mix_hash(&number);
      
         let mut num = number;
@@ -181,13 +112,15 @@ impl MyBloomFilter{
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
+    use super::*;
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};  
     #[test]
+    //Read 10 000 000 numbers from a file and insert them in the bloom filter.
+    //Then ask for them and assert that u take 10 000 000 yes.
     fn bloom_correctness(){
         
-        //Read 10 000 000 numbers from a file and insert them in the bloom filter.
-        //Then ask for them and assert that u take 10 000 000 yes.
 
         let mut bloom = MyBloomFilter::create(10000000 as u64,0.00001 as f64);
         let file = File::open("./input/bloom_input_10m.txt").expect("error in opening bloom_input file");
@@ -199,7 +132,7 @@ mod tests {
                     Err(why) => panic!("{:?}", why),
                     Ok(string) => hash = string.parse::<u64>().unwrap()
                 }
-                bloom.clever_insert_bloom(hash); 
+                bloom.insert_bloom(hash); 
         });
 
        
