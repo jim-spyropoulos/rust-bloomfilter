e52c4cfd171337b00a1ecbebb3fc58372dbd4a92
Dimitris <dimitrisspyr@gmail.com>
2019-07-03 14:38:26 +0300

    Licensed Code

--- src/double.c
+++
@@ -1,9 +0,0 @@
-#include "double.h"
-
-int plus_one(int input){
-    return input +1 ;
-}
-unsigned long long double_input(int input) {
-    return plus_one(input) * 2;
-}
-
--- src/double.h
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-
-unsigned long long double_input(int input);
